ac73767d4498adca529488aa8276a0790946aa69
ayushi023 <ayushi023>
2021-05-18 00:27:25 +0530

    344. Reverse String

---
+++ 344._Reverse_String.java
@@ -0,0 +1,26 @@
+/*
+
+Write a function that reverses a string. The input string is given as an array of characters s. Example 1:Example 2: Constraints: Follow up: Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+
+
+*/
+
+/*
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+
+
+*/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    
+};
+​
